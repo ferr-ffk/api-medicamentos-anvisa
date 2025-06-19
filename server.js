fc099cc1f08568456f114244cb3d257bbcb20200
Fernando Freitas <freitax.li@gmail.com>
2025-06-19 18:30:46 -0300

    feat(express): adicionar cors apra acesso público da api

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require('cors')
+
 const app = express();
 const port = 8080;
+
+app.use(cors())
 
 function readTextFile(file, callback) {
     fetch(file)
