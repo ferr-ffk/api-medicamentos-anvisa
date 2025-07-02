53ba05af5caf88d55bd78e55047b9cd127655444
Fernando Freitas de Lira <freitax.li@gmail.com>
2025-07-02 11:18:39 -0300

    feat(routes): adicionar rota de resultados simplificados

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 8080;
 app.use(cors())
 
 function readTextFile(file, callback) {
-    fetch(file)
+    fetch(file, { method: 'GET', headers: { 'Content-Type': 'text/plain' } })
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -28,6 +28,7 @@ app.get("/", (req, res) => {
 
 app.get("/:nome", (req, res) => {
     const nome = req.params.nome;
+    const simplified = req.query.simplified;
 
     if (nome === "favicon.ico") {
         res.status(204).send(); // No content for favicon requests
@@ -36,6 +37,37 @@ app.get("/:nome", (req, res) => {
     }
 
     console.log("Pesquisa por medicamento:", nome);
+    console.log("Simplified:", simplified);
+
+    if (simplified) {
+        readTextFile("https://ferr-ffk.github.io/medicamentos/medicamentos-reduzido.json", function(text){
+            const data = JSON.parse(text);
+    
+            console.log("Dados carregados:", data.length, "medicamentos encontrados");
+    
+            let possiveisMedicamentos = [];
+            let limit = 0;
+    
+            data.forEach((medicamento) => {
+                if (medicamento['NOME_PRODUTO'].toLowerCase().includes(nome.toLowerCase())) {
+                    possiveisMedicamentos.push(medicamento);
+                }
+            });
+    
+            if (req.query.limit > 0) {
+                limit = parseInt(req.query.limit);
+    
+                possiveisMedicamentos = possiveisMedicamentos.slice(0, limit);
+            }
+
+            console.log("Possíveis medicamentos encontrados:", possiveisMedicamentos.length);
+    
+            res.json(possiveisMedicamentos);
+        });
+
+        return;
+    }
+
 
     readTextFile("https://ferr-ffk.github.io/medicamentos/medicamentos.json", function(text){
         const data = JSON.parse(text);
@@ -58,8 +90,10 @@ app.get("/:nome", (req, res) => {
         }
 
         res.json(possiveisMedicamentos);
+
+        return;
     });
-    
+
 });
 
 app.listen(port, () => {
